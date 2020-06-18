8fed2f9ff1be2dd2a701810fec3a2d904a2456ee
CrKot <crkoteika@gmail.com>
2020-06-18 23:45:48 +0700

    fix cognitive complexity

--- games/even.js
+++ games/even.js
@@ -4,17 +4,18 @@ import {
   random, userName, falseAnswer,
 } from '../src/index.js';
 
+const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+
 const evenOrNotEven = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const num = random(0, 100);
-    const isEven = (num % 2 === 0) ? 'yes' : 'no';
     console.log(`Question: ${num}`);
     const yourAnswer = readlineSync.question('Your answer: ');
-    if (isEven === yourAnswer) {
+    if (isEven(num) === yourAnswer) {
       console.log('Correct!');
     } else {
-      return falseAnswer(yourAnswer, isEven);
+      return falseAnswer(yourAnswer, isEven(num));
     }
   }
   return console.log(`Congratulations, ${userName}!`);
